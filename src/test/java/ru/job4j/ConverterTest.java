3a5869fd00ec1417659624cebe7dbd26a4d378d7
Andrew <andreymustafin.it@gmail.com>
2021-10-17 20:15:17 +0300

    1. Модульные тесты. [#159435] fix3

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.converter;
 
 import org.testng.annotations.Test;
-//import static org.junit.Assert.*;
 
 public class ConverterTest {
 
@@ -17,7 +16,7 @@ public class ConverterTest {
     @Test
     public void whenConvert180RblThen3Dlr() {
         float in = 180;
-        float expected = 2;
+        float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
