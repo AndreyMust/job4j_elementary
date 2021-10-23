00e46f8751c41387bc90d8dedd98f1a08e1956d0
Andrew <andreymustafin.it@gmail.com>
2021-10-23 23:56:37 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryOne = new Battery(45);
+        Battery batteryTwo = new Battery(35);
+        System.out.println("Заряд батареи 1: " + batteryOne.load);
+        System.out.println("Заряд батареи 2: " + batteryTwo.load);
+        System.out.println("Идет Перезарядка...");
+        batteryOne.exchange(batteryTwo);
+        System.out.println("Перезарядка Завершена.");
+        System.out.println("Заряд батареи 1: " + batteryOne.load);
+        System.out.println("Заряд батареи 2: " + batteryTwo.load);
+    }
+}
