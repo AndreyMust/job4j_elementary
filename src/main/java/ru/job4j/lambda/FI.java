e647bf5f023c20cf1c24eb16f429ba7b6f1a913a
Andrew <andreymustafin.it@gmail.com>
2021-11-18 11:25:15 +0300

    2.1. Лямбда [#58165 #229778]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,35 @@
+package ru.job4j.lambda;
+
+import java.util.*;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 333", 120),
+                new Attachment("image 22", 23)
+        };
+        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
+        Arrays.sort(atts, comparator);
+        for (Attachment a : atts) {
+            System.out.println(a);
+        }
+
+        String[] arrString = {
+                new String("Name 1"),
+                new String("Name 333"),
+                new String("Name 22")
+        };
+        Comparator<String> cmpText  = (left, right) -> left.compareTo(right);
+        Arrays.sort(arrString, cmpText);
+        for (String s : arrString) {
+            System.out.println(s);
+        }
+
+        Comparator<String> cmpDescSize  = (left, right) ->  Integer.compare(right.length(), left.length());
+        Arrays.sort(arrString, cmpDescSize);
+        for (String s : arrString) {
+            System.out.println(s);
+        }
+    }
+}
