661c200e6e5971898492583e7f76f9c55935af98
Andrew <andreymustafin.it@gmail.com>
2021-11-18 23:40:23 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+    }
+}
